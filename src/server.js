e5f533c7229ee71efd816f72dfbbed0c509d6d1a
Charles Kato <kcharles52@gmail.com>
2019-09-17 11:11:04 +0300

    feature(routes): add route
    - add product routes
    - add orders routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
-const http = require("http");
-const app = require("./app");
+const http = require('http');
+const app = require('./app');
 
 const port = process.env.PORT || 4500;
 
 const server = http.createServer(app);
 
-server.listen(port);
+const start = () => {
+  try {
+    server.listen(port, () => {
+      console.log(`REST API on http://localhost:${port}/api`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = start;
