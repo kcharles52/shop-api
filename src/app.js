16cce5a6ca279e52176d0870d0fe0431a025f738
Kato Charles <kcharles52@gmail.com>
2019-09-18 14:18:45 +0300

    chore(error): error handling (#3)
    - add middleware to trap errors
    - add middleware to get json body data
    - enable CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,53 @@
+// Third party libraries
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const { json, urlencoded } = require('body-parser');
 
+// modules
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/orders');
 
+// create express app
+const app = express();
+
+// app middleware
+app.use(morgan('dev'));
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Types, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes for handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// for error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
